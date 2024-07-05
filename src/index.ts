7eb9130652f774f6139163eca99d47e3338e98db
davide <davide.debernardi@bevy-express.com>
2024-07-05 17:03:57 +0200

    TECH-981 Export tracing, bump version

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,6 @@ import {ConsoleLoggerAdapter} from "./log/console-logger-adapter";
 const adapter = process.env.CLOUD_LOGGER_CONSOLE_FALLBACK
     ? new ConsoleLoggerAdapter()
     : new GcpLoggerAdapter(new GcpWrapper(), LoggingOptionsDefaults);
+
+export {tracingMiddleware, getTraceId, getTraceHeader} from "./tracing";;
 export const logger = new Logger(adapter);
