e584699ef4f0c87762fa71bb20f13f15a2cf0692
davide <davide.debernardi@bevy-express.com>
2024-07-08 09:56:00 +0200

    TECH-981 Fix trace-id

--- src/tracing/index.ts
+++ src/tracing/index.ts
@@ -5,6 +5,8 @@ import { Request, Response, NextFunction } from 'express';
 const createNamespace = require('continuation-local-storage').createNamespace;
 const ns = createNamespace('logger-context');
 const headerName: string = 'X-Cloud-Trace-Context';
+const traceIdKey = 'trace-id';
+const traceHeaderKey = 'trace-header';
 const project = process.env.GOOGLE_CLOUD_PROJECT;
 
 axios.interceptors.request.use((conf) => {
@@ -12,15 +14,15 @@ axios.interceptors.request.use((conf) => {
     return conf;
 });
 
-export const getTraceId = () => `projects/${project}/traces/${ns.get('trace')}`;
-export const getTraceHeader = () => ns.get('trace-header');
+export const getTraceId = () => `projects/${project}/traces/${ns.get(traceIdKey)}`;
+export const getTraceHeader = () => ns.get(traceHeaderKey);
 export const expressTracingMiddleware = (req: Request, res: Response, next: NextFunction) => {
     ns.run(() => {
         const header: string | undefined = req.header(headerName);
         const chunks: string[] | undefined = header?.split(';');
         if (chunks) {
-            ns.set('trace-header', header ?? randomUUID());
-            ns.set('trace-id', chunks[0]);
+            ns.set(traceHeaderKey, header ?? randomUUID());
+            ns.set(traceIdKey, chunks[0]);
         }
         next();
     });
